2d70d9135a8b4a6516d209822e808400b8f0c3b2
hemantgovekar <hemantgovekar@gmail.com>
2017-03-31 17:16:39 -0500

    initial commit

---
+++ SimpleWebApplication/Scripts/script.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.js" />
+
+var url = "http://localhost:3606/api/employee/"
+var myApp = angular.module("myApp", []);
+
+var MainController = function ($scope, $http) {
+
+    var onSucess = function (response) { $scope.employees = response.data };
+
+    var onFailure = function (reason) { $scope.error = reason };
+
+    var getAllEmployees = function () {
+        $http.get(url)
+        .then(onSucess, onFailure)
+    };
+
+    getAllEmployees();
+
+};
+
+myApp.controller("MainController", MainController);
---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace WebApi.Controllers
+{
+    //[Authorize]
+    public class EmployeeController : ApiController
+    {
+        // GET api/Employee
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDbEntities employeeDbEntities = new EmployeeDbEntities())
+            {
+                return employeeDbEntities.Employees.ToList();
+            }
+        }
+
+        // GET api/Employee/5
+        public Employee Get(int id)
+        {
+            using (EmployeeDbEntities employeeDbEntities = new EmployeeDbEntities())
+            {
+                return employeeDbEntities.Employees.FirstOrDefault(e => e.Id == id);
+            }
+        }
+
+        // POST api/Employee
+        public void Post([FromBody]Employee value)
+        {
+            using (EmployeeDbEntities employeeDbEntities = new EmployeeDbEntities())
+            {
+                employeeDbEntities.Employees.Add(value);
+                employeeDbEntities.SaveChanges();
+            }
+        }
+
+        // PUT api/Employee/5
+        public void Put(int id, [FromBody]Employee value)
+        {
+            using (EmployeeDbEntities employeeDbEntities = new EmployeeDbEntities())
+            {
+                Employee employee = employeeDbEntities.Employees.FirstOrDefault(e => e.Id == id);
+                employee.EName = value.EName;
+                employeeDbEntities.SaveChanges();
+            }
+        }
+
+        // DELETE api/Employee/5
+        public void Delete(int id)
+        {
+            using (EmployeeDbEntities employeeDbEntities = new EmployeeDbEntities())
+            {
+                Employee employee = employeeDbEntities.Employees.FirstOrDefault(e => e.Id == id);
+                employeeDbEntities.Employees.Remove(employee);
+                employeeDbEntities.SaveChanges();
+            }
+        }
+    }
+}
